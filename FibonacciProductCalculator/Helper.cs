bd75eb33587a645e3385eade1409ed2e477d40f6
SwapnilShingade <shingadeswsapnil23@gmail.com>
2020-08-13 21:27:50 +0530

    Added all the files

---
+++ FibonacciProductCalculator/Helper.cs
@@ -0,0 +1,118 @@
+﻿using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FibonacciProductCalculator
+{
+    public static class Helper
+    {
+        public  static readonly string ExceptionMessage = "Exception Occured in method: {0} exception message: {1}";
+        /// <summary>
+        /// return product of numbers in fibonacci series
+        /// upto entered limitingNumber
+        /// </summary>
+        /// <param name="limitingNumber"></param>
+        /// <returns>int</returns>
+        public static int ProcessFibonacciSeries(int limitingNumber, ILogger _logger)
+        {
+            int fibonacciProductSeries = 1;
+            try
+            {
+                int firstNumber = 1, secondNumber = 1, calculatedValue;                
+                string fibonacciSeries = firstNumber + "," + secondNumber;
+
+                for (int i = 2; i < limitingNumber; ++i)
+                {
+                    calculatedValue = firstNumber + secondNumber;
+                    fibonacciSeries = fibonacciSeries + "," + calculatedValue;
+                    fibonacciProductSeries *= calculatedValue;
+                    firstNumber = secondNumber;
+                    secondNumber = calculatedValue;
+                }
+
+                Console.WriteLine("\n" + $"Fibonacci Number Series upto {limitingNumber}: {fibonacciSeries}");                
+                Console.WriteLine("\n" + $"Product of Fibonacci Numbers upto {limitingNumber}: {fibonacciProductSeries}");                
+            }
+            catch(Exception ex)
+            {
+                _logger.LogError(string.Format(ExceptionMessage, "ProcessFibonacciSeries", ex.Message));
+            }
+            return fibonacciProductSeries;
+        }
+
+        /// <summary>
+        /// Check if user wants to conitnue
+        /// </summary>
+        /// <returns>bool</returns>
+        public static bool PromptForContinuing()
+        {
+            Console.WriteLine("\n" + "Do you wish to continue? Please enter " + "Y" + " for Yes " + "N" + " for No");
+            var isContinuedByUser = Console.ReadKey();
+            if ("y" == isContinuedByUser.KeyChar.ToString().ToLower())
+                return true;
+            else return false;
+        }
+
+        /// <summary>
+        /// Ask user for limitingNumber for Fibonacci Series
+        /// </summary>
+        /// <returns>string</returns>
+        public static string PromptForValidInput()
+        {
+            Console.WriteLine("Please enter a limiting number for Fibonacci Series, number must be greater than zero.");
+            return Console.ReadLine();
+        }
+
+
+        /// <summary>
+        /// Ask user for limitingNumber for Fibonacci Series
+        /// </summary>
+        /// <returns>int</returns>
+        public static int CheckForRetry(ILogger _logger)
+        {
+            int result =  0;
+            try
+            {
+                Console.WriteLine("\n" + "Please enter a limiting number for Fibonacci Series, number must be greater than zero.");
+                string userInput = Console.ReadLine();
+                int limitingNumber = Convert.ToInt32(userInput);
+                return ProcessFibonacciSeries(limitingNumber, _logger);              
+
+            }
+            catch(Exception ex)
+            {
+                _logger.LogError(string.Format(ExceptionMessage, "ProcessFibonacciSeries", ex.Message));
+                Console.WriteLine("Please Enter Valid Input!!");
+            }
+            return result;
+        }
+
+
+        /// <summary>
+        /// Writes final series of Product of Numbers from Fibonacci Series
+        /// </summary>
+        /// <param name="product"></param>
+        /// <param name="finalSeriesOfProduct"></param>
+        public static void FinalResult(int product, ILogger _logger, ref string finalSeriesOfProduct)
+        {
+            try
+            {
+                if (string.IsNullOrEmpty(finalSeriesOfProduct))
+                {
+                    finalSeriesOfProduct = finalSeriesOfProduct + product;
+                }
+                else
+                {
+                    finalSeriesOfProduct = finalSeriesOfProduct + "," + product;
+                }
+            }
+
+            catch(Exception ex)
+            {
+                _logger.LogError(string.Format(ExceptionMessage, "FinalResult", ex.Message));
+            }
+            Console.WriteLine("\n" + "Series of Product of Fibonacci Numbers: " +finalSeriesOfProduct);
+        }
+    }
+}
---
+++ FibonacciProductCalculator/Program.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+
+namespace FibonacciProductCalculator
+{
+    class Program
+    {
+        public static ILogger<Program> _logger;
+        static void Main(string[] args)
+        {
+            try
+            {
+                // Add Service Collection to configure Configuration Services
+                var serviceCollection = new ServiceCollection();
+                ConfigureServices(serviceCollection);
+                IServiceProvider serviceProvider = serviceCollection.BuildServiceProvider();
+                _logger = serviceProvider.GetService<ILogger<Program>>();                
+
+                Console.WriteLine("***Welcome to Fibonacci Series Product Calculator!!***");
+                int limitingNumber, productOfAll;
+                string userInput = string.Empty;
+                string finalSeriesOfProduct = string.Empty;
+                var isContinued = false;                
+
+                // Check if user enters valid value
+                do
+                {
+                    userInput = Helper.PromptForValidInput();
+                    limitingNumber = Convert.ToInt32(userInput);
+                }
+                while (limitingNumber <= 0);
+
+                productOfAll = Helper.ProcessFibonacciSeries(limitingNumber, _logger);                
+                finalSeriesOfProduct = "" + productOfAll;
+
+                // Check if user wants to enter number again
+                do
+                {
+                    if (isContinued = Helper.PromptForContinuing())
+                    {
+                        productOfAll = Helper.CheckForRetry(_logger);
+                    }
+                        
+                   Helper.FinalResult(productOfAll, _logger, ref finalSeriesOfProduct);                     
+                    
+                }
+                while (isContinued);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError("Exception Occured: " + ex.Message);
+                Console.WriteLine("Exception Occured " +ex.Message);
+            }
+        }
+
+        /// <summary>
+        /// Configure Logger
+        /// </summary>
+        /// <param name="serviceCollection"></param>
+        private static void ConfigureServices(IServiceCollection serviceCollection)
+        {
+            serviceCollection.AddLogging(configure => configure.AddConsole())
+               .AddTransient<Program>();
+        }
+    }
+}
